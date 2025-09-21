55d42fc4f2d963cd928e92e98550c807e2873f4b
pvtpylot <75529696+pvtpylot@users.noreply.github.com>
2025-09-21 17:40:43 -0400

    Addding the show areas

---
+++ MauiBlazorWeb/MauiBlazorWeb.Web/Components/Layout/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MauiBlazorWeb.Shared.Models.DTOs
+{
+    public class UserDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool EmailConfirmed { get; set; }
+        public bool IsLockedOut { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
